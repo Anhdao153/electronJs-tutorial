9083818c65012710b5947877562df61d9cb8d165
Anhdao153 <ndthien.1532000@gmail.com>
2022-11-13 17:15:21 +0700

    Config: Add hot Reload

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 
 const { app, BrowserWindow, ipcMain } = require('electron')
+
+const path = require('path')
+
+// If development environment
+// if (env === 'development') {
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+  hardResetMethod: 'exit'
+})
+// }
+
 const windowStateKeeper = require('electron-window-state')
 // const colors = require('colors')
 // include the Node.js 'path' module at the top of your file
